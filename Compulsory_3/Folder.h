afdc8986fa29bf87060942939b6bac9181a49f1e
Anda <andzelikawos56@gmail.com>
2023-10-31 15:02:23 +0100

    idk date, but random size work

--- Compulsory_3/Folder.h
+++ Compulsory_3/Folder.h
@@ -1,19 +1,30 @@
 #pragma once
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <ctime>
 using namespace std;
 
 class Folder 
 {
 private:
 	string name;
-	int size, date;
+	int size;
+	time_t date;
+	int tm_mday, tm_mon;
 
 public:
 	Folder(string newName, int newSize, int newDate) 
 	{
 		name = newName;
-		size = newSize;
-		date = newDate;
+
+		srand(time(NULL));
+		size = rand() % 100 + 1;
+		cout << size;
+
+		date = time(0);
+		char* dt = ctime(&date);
+		cout << "date: " << dt << endl;
 	}
 
 	string getName() 
@@ -27,5 +38,17 @@ public:
 	int getDate()
 	{
 		return date;
+	}
+
+	void print() 
+	{
+		cout << "Folder name: " << name << endl;
+		cout << "Folder size: " << size << "MB" << endl;
+		cout << "Folder date: " << date << endl;
+	}
+
+	void print(int size) 
+	{
+		cout << "size: " << getSize();
 	}
 };
--- Compulsory_3/Main.cpp
+++ Compulsory_3/Main.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 int main() 
 {
+	Folder dog("Cute_dogos", 0, 0);
+	dog.print();
 
 }
